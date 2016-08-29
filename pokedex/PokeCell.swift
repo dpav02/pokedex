7653defa36327875ee8ae6903a7ad99ee479439b
dpav02 <dpav02@gmail.com>
2016-08-29 19:12:22 -0400

    Added a class for the pokemon cell in the collection view

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by David Pavel on 8/29/16.
+//  Copyright © 2016 David Pavel. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
